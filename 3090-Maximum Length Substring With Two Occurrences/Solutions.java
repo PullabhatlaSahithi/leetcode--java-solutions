cbb10606755d4763d2110c31ec1516dfc9f48f64
sahithi pullabhatla <sahithipullabhatla@gmail.com>
2026-08-14 21:40:18 +0530

    Add method to find maximum length substring with two characters
    Implement maximum length substring method to find the longest substring with at most two distinct characters.

---
+++ 3090-Maximum Length Substring With Two Occurrences/Solutions.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int maximumLengthSubstring(String s) {
+        HashMap<Character,Integer> map=new HashMap<>();
+        int left=0;
+        int maxlen=0;
+        for(int i=0;i<s.length();i++)
+        {
+            char ch=s.charAt(i);
+            map.put(ch,map.getOrDefault(ch,0)+1);
+
+            while(map.get(ch)>2)
+            {
+                char leftchar=s.charAt(left);
+                map.put(leftchar,map.get(leftchar)-1);
+                left++;
+            }
+            maxlen=Math.max(maxlen,i-left+1);
+        }
+        return maxlen;  
+    }
+}
